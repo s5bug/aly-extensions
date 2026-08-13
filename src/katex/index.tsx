2526868ae4fc953eacc089f4b71af24aedeecaef
Aly <me@s5.pm>
2026-08-13 13:47:30 -0600

    fix crash with missing react

--- src/katex/index.tsx
+++ src/katex/index.tsx
@@ -2,7 +2,7 @@ import type { ExtensionWebpackModule } from '@moonlight-mod/types'
 
 export const webpackModules: Record<string, ExtensionWebpackModule> = {
   entrypoint: {
-    dependencies: [{ ext: 'markdown', id: 'markdown' }],
+    dependencies: [{ id: 'react' }, { ext: 'markdown', id: 'markdown' }],
     entrypoint: true,
     // There is no `run` specified here, as this Webpack module is stored in a
     // separate file. See `src/katex/webpackModules/entrypoint.ts`.
